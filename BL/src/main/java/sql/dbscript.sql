018d211c6f5d6ea8ba9ab51d28e27827bfb6c007
Frame_Thanadet <625021000108@mail.rmutk.ac.th>
2022-12-26 10:38:48 +0700

    Update dbscript.sql

--- BL/src/main/java/sql/dbscript.sql
+++ BL/src/main/java/sql/dbscript.sql
@@ -238,3 +238,11 @@ PRIMARY KEY (`file_id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 --04/10/2022 BEST change name
 ALTER TABLE `employee` CHANGE `image` `file_id` VARCHAR(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
 ALTER TABLE `sys_user` CHANGE `image` `file_id` VARCHAR(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+
+--26/12/2565 Frame add columns
+ALTER TABLE `quotation` ADD `quotation_sale_id` VARCHAR(32) NOT NULL AFTER `company_id`, 
+ADD `doc_status_id` VARCHAR(32) NOT NULL AFTER `quotation_sale_id`, 
+ADD `quotation_doc_id` VARCHAR(1024) NOT NULL AFTER `doc_status_id`, 
+ADD `revision` VARCHAR(8) NOT NULL AFTER `quotation_doc_id`;
+-- 26/12/2022 Frame change Table name
+ALTER TABLE status RENAME TO doc_status
